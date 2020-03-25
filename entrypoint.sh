3b81bae2c31d57c51a1e7cbbc893812226d62378
Matthew Morse <mmorse1217@gmail.com>
2020-03-25 00:30:08 -0400

    debug github actions.

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,8 @@
 
 # code to execute during CI build
 mkdir -p /patchwork/build/ 
+ls /patchwork
+ls /patchwork/cmake
 cd /patchwork/build/
 export BLENDSURF_DIR=/libs/blendsurf 
 export P4EST_DIR=/libs/p4est-1.1

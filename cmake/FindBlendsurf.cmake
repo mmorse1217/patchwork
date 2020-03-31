fe54d2477539d29eb226330f7df761a7d2e537c6
Matthew Morse <mmorse1217@gmail.com>
2020-03-31 14:37:54 -0400

    added install code to CMakeLists.txt.

--- cmake/FindBlendsurf.cmake
+++ cmake/FindBlendsurf.cmake
@@ -10,6 +10,24 @@ if(Blendsurf_FOUND)
     return()
 endif()
 
+file(GLOB lib_glob "/usr/local/lib/blendsurf*")
+file(GLOB inc_glob "/usr/local/include/blendsurf*")
+find_library(Blendsurf_LIB 
+    NAMES blendsurf
+    HINTS
+        ${Blendsurf_DIR}
+        ${BLENDSURF_DIR}
+        $ENV{Blendsurf_DIR}
+        $ENV{BLENDSURF_DIR}
+        ENV BLENDSURF_DIR
+    PATHS
+        /usr
+        /usr/local
+        /usr/local/lib
+        ${lib_glob}
+    PATH_SUFFIXES 
+        lib
+)
 find_path(Blendsurf_INCLUDE_DIR bdsurf.hpp
     HINTS
         ${Blendsurf_DIR}
@@ -18,26 +36,32 @@ find_path(Blendsurf_INCLUDE_DIR bdsurf.hpp
         $ENV{BLENDSURF_DIR}
         ENV BLENDSURF_DIR
     PATHS
-        ${CMAKE_SOURCE_DIR}/../..
-        ${CMAKE_SOURCE_DIR}/..
-        ${CMAKE_SOURCE_DIR}
-        ${CMAKE_SOURCE_DIR}/blendsurf
-        ${CMAKE_SOURCE_DIR}/../blendsurf
-        ${CMAKE_SOURCE_DIR}/../../blendsurf
         /usr
         /usr/local
-    PATH_SUFFIXES include
+        /usr/local/include
+        ${inc_glob}
+    PATH_SUFFIXES 
+        include
 )
 
 
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(Blendsurf
-    "\nBlendsurf not found --- You can download it using:\n\tgit clone https://github.com/mmorse1217/blendsurf\n and setting the BLENDSURF_DIR environment variable accordingly"
-    Blendsurf_INCLUDE_DIR)
-mark_as_advanced(Blendsurf_INCLUDE_DIR)
+    "\nBlendsurf not found --- You can download it using:\n\tgit clone 
+    https://github.com/mmorse1217/blendsurf\n and setting the BLENDSURF_DIR environment variable accordingly"
+    Blendsurf_LIB Blendsurf_INCLUDE_DIR)
+mark_as_advanced(Blendsurf_INCLUDE_DIR Blendsurf_LIB)
 
-set(Blendsurf_LIB "${Blendsurf_INCLUDE_DIR}/../lib/"  )
-set(Blendsurf_LIBRARIES "${Blendsurf_INCLUDE_DIR}/../lib/libblendsurf.a"  )
-set(Blendsurf_render_LIBRARIES "${Blendsurf_INCLUDE_DIR}/../lib/libblendsurf_render.a"  )
-set(Blendsurf_render_INCLUDE_DIR "${Blendsurf_INCLUDE_DIR}/../vis/"  )
-list(APPEND CMAKE_MODULE_PATH "${Blendsurf_INCLUDE_DIR}/../cmake")
+set(Blendsurf_INCLUDE_DIRS ${Blendsurf_INCLUDE_DIR})
+set(Blendsurf_LIBRARIES ${Blendsurf_LIB})
+#set(Blendsurf_LIBRARIES "${Blendsurf_LIB}/../lib/libblendsurf.a"  )
+#set(Blendsurf_render_LIBRARIES "${Blendsurf_LIB}/../lib/libblendsurf_render.a"  )
+#set(Blendsurf_render_INCLUDE_DIR "${Blendsurf_LIB}/../vis/"  )
+
+message("found blendsurf: " ${Blendsurf_FOUND})
+if(Blendsurf_FOUND AND NOT TARGET Blendsurf::Blendsurf)
+    add_library(Blendsurf::Blendsurf STATIC IMPORTED)
+  set_target_properties(Blendsurf::Blendsurf PROPERTIES
+    INTERFACE_INCLUDE_DIRECTORIES  "${BLENDSURF_INCLUDE_DIRS}"
+    IMPORTED_LOCATION "${BLENDSURF_LIBRARIES}")
+endif()
